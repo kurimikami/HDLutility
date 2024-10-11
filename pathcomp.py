e32f108ef1d2deef6340cbd87bd428299382433f
三上浩 <kuri.mikami@gmail.com>
2024-10-11 19:28:30 +0900

    update

--- pathcomp.py
+++ pathcomp.py
@@ -3,6 +3,11 @@ import argparse
 import re
 import os
 
+Black  = "\033[0m"
+Red    = "\033[31m"
+Yellow = "\033[33m"
+Cyan   = "\033[36m"
+
 # Remove comments, backslashes, and whitespace
 def clean_path(line):
     # Remove comments
@@ -12,54 +17,71 @@ def clean_path(line):
     line = line.replace('\\', '')  
     return line
 
-def resolve_symlink(path):
-    """
-    Follows the nesting of symbolic links, expands environment variables if present,
-    and returns the final target file path.
-    """
+def find_vhdl_files(directory):
+    vhdl_extensions = {'.vhd', '.v', '.svh', '.sv'}
+    found_files = []
+
+    for root, dirs, files in os.walk(directory):
+        for filename in files:
+            if os.path.splitext(filename)[1] in vhdl_extensions:
+                found_files.append(resolve_symlink(os.path.join(root, filename)))
+
+    return found_files
+
+def resolve_symlink(path, expand=False):
+
     try:
         # Expand environment variables
-        expanded_path = os.path.expandvars(path)
+        if expand:
+            path = os.path.expandvars(path)
         # Get the actual path that the link ultimately points to (file/directory)
-        real_path = os.path.realpath(expanded_path)
+        real_path = os.path.realpath(path)
         return real_path
     except Exception as e:
         return str(e)
 
 # Read the file and create a cleaned-up PATH list
-def read_paths(filename):
+def read_paths(filename, expand=False):
     paths = {}
     linenum = 0
     # ext_pattern = r"(?<=[$/\w\W])\s*([^'\"\[\];\s]+\.(vhd|v|sv|svh))\b"
     ext_pattern = r"(?:\s*[=|'|\"|\[|+]\s*)([^'\"=\s]+\.(?:vhd|sv|v|svh))\b(?:\s*\w*)"
+
+    if not os.path.exists(filename):
+        print(f"Error: File '{filename}' does not exist.")
+        return paths
     
-    with open(filename, 'r') as file:
-        for line in file:
-            linenum += 1
-    
-            fullpaths = re.findall(ext_pattern, clean_path(line))
-            if fullpaths:
-                for fullpath in fullpaths :
-                    filename = os.path.basename(fullpath)
-                    paths[filename] = {'LN': str(linenum).rjust(10), 'PATH': resolve_symlink(fullpath)}
+    try:
+        with open(filename, 'r') as file:
+            for line in file:
+                linenum += 1
+        
+                fullpaths = re.findall(ext_pattern, clean_path(line))
+                if fullpaths:
+                    for fullpath in fullpaths :
+                        basename = os.path.basename(fullpath)
+                        if basename in paths :
+                            print(Yellow + '#REPL : ' + Black + basename + '(' + filename + ')')
+                        paths[filename] = {'LN': str(linenum).rjust(10), 'PATH': resolve_symlink(fullpath, expand)}
+    except IOError as e:
+        print(f"Error: Unable to read file '{filename}'. {str(e)}")
 
     return paths
 
 # Display the differences
 def compare_paths(paths_a, paths_b):
-    Black = "\033[0m"
-    Red   = "\033[31m"
-    for filename, fullpath in paths_b.items():
+    for filename in list(paths_b.keys()):
         filename = paths_a.get('PATH', {}).get('PATH', None)
         if filename:
             if paths_a[filename]['PATH'] != paths_b[filename]['PATH']:
                 print(Black + '#UM A :' + paths_a[filename]['LN'] + ' ' + paths_a[filename]['PATH'])
                 print(Red   + '#UM B :' + paths_b[filename]['LN'] + ' ' + paths_b[filename]['PATH'])
         else:
-            print('#xA B ', paths_b[filename]['PATH'])
-    for filename, fullpath in paths_a.items():
+            print(Cyan + '#xA B ', paths_b[filename]['PATH'])
+
+    for filename in list(paths_a.keys()):
         if filename not in paths_b:
-            print('#xB A ', paths_a[filename]['PATH'])
+            print(Black + '#xB A ', paths_a[filename]['PATH'])
 
 # Export information from file A
 def export_paths(paths_a):
@@ -73,11 +95,12 @@ def main():
     parser.add_argument('file_a', help='file A')
     parser.add_argument('file_b', nargs='?', help='file B', default=None)
     parser.add_argument('--export', action='store_true', help='print paths of file A')
+    parser.add_argument('--expand', action='store_true', help='expand environment variables in file paths')
     
     args = parser.parse_args()
 
     # Create a PATH list for file A
-    paths_a = read_paths(args.file_a)
+    paths_a = read_paths(args.file_a, expand=args.expand)
 
     if args.export:
         # If --export is specified, export only the paths from file A
@@ -85,10 +108,11 @@ def main():
     else:
         # If --export is not specified, compare file A with file B
         if args.file_b:
-            paths_b = read_paths(args.file_b)
-            compare_paths(paths_a, paths_b)
+            paths_b = read_paths(args.file_b, expand=args.expand)
+            compare_paths(paths_a, paths_b) 
         else:
             print("Error")
+            parser.print_usage()
 
 if __name__ == "__main__":
     main()
